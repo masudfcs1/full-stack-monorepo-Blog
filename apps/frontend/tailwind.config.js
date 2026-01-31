3216d761582fc1f543b59721aa1b105f0b70e8d5
masudfcs1 <masudfcs1@gmal.com>
2026-01-31 18:02:42 +0600

    tailwind css update

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Only if using /src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
